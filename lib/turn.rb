3a34c0b814544ae7fd3e728055a18670a007232a
Jim Riddle <jimriddle0@gmail.com>
2022-03-30 12:07:16 -0500

    notes: added some text on what I think a turn should do

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,17 @@ attr_reader :board
     @board = board
   end
 
+  # def takes_turn
+  #   player takes a turn, recieves input
+  #     checks to make sure it is valid input
+  #     if the column is full, let the player know that the column is full
+  #       if the input is invalid (X) for example, let the player know that we need a correct letter
+  #     if it's good to go places the tile
+  #   computer takes a turn (logic already built in to make sure that the list of available columns is only valid columns)
+  #   checks winning conditions / end conditions
+  #   says keep playing, or player 1 has won... or draw
+  # end
+
 
 
 
