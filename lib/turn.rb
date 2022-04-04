b7dca327706ac4d3df9ae605e9bae6c9abb1b586
Andrew King <dev.andrewking@gmail.com>
2022-04-04 14:19:16 -0500

    refactor: rev2

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,3 @@
-# require '.lib/board'
-# require 'pry'
 class Turn
 
 attr_reader :board, :game_over
@@ -9,7 +7,6 @@ attr_reader :board, :game_over
   end
 
   def takes_turn
-    # binding.pry
     board.player_turn
     if board.board_full == true
       puts ""
@@ -63,80 +60,19 @@ attr_reader :board, :game_over
   def check_horizontal
     winner_array = []
     index_counter = 0
-    7.times do
+    7.times do |index|
       check_array = []
       board.board_hash.keys.each do |key|
-        check_array << (board.board_hash[key][index_counter])
+        check_array << (board.board_hash[key][index])
       end
-      index_counter += 1
+      #index_counter += 1
       winner_array << check_win_condition(check_array)
     end
     check_winner(winner_array)
   end
 
-  def check_winner(winner_array)
-    if winner_array.include?("X")
-      winner = "X"
-    elsif winner_array.include?("O")
-      winner = "O"
-    else
-      winner = ""
-    end
-    return winner
-  end
-
-  # def check_diagonal_up_right
-  #   winner_array = []
-  #   winner = ""
-  #   custom_key_array = [6, 6, 6, 5, 4, 3]
-  #   starting_index_array = [2, 1, 0, 0, 0, 0]
-  #   length_array = [4, 5, 6, 6, 5, 4]
-  #     6.times do
-  #       check_array = []
-  #       index_counter = starting_index_array[0]
-  #       key_counter = custom_key_array[0]
-  #       length_array[0].times do
-  #         check_array << (board.board_hash[board.board_hash.keys[key_counter]][index_counter])
-  #         index_counter += 1
-  #         key_counter -= 1
-  #       end
-  #       winner_array << check_win_condition(check_array)
-  #       custom_key_array.shift
-  #       starting_index_array.shift
-  #       length_array.shift
-  #     end
-  #   check_winner(winner_array)
-  # end
-
-  # def check_diagonal_down_right
-  #   winner_array = []
-  #   winner = ""
-  #   custom_key_array = [0, 0, 0, 1, 2, 3]
-  #   starting_index_array = [2, 1, 0, 0, 0, 0]
-  #   length_array = [4, 5, 6, 6, 5, 4]
-  #     6.times do
-  #       check_array = []
-  #       index_counter = starting_index_array[0]
-  #       key_counter = custom_key_array[0]
-  #
-  #       length_array[0].times do
-  #
-  #         check_array << (board.board_hash[board.board_hash.keys[key_counter]][index_counter])
-  #         index_counter += 1
-  #         key_counter += 1
-  #
-  #       end
-  #       winner_array << check_win_condition(check_array)
-  #       custom_key_array.shift
-  #       starting_index_array.shift
-  #       length_array.shift
-  #     end
-  #   check_winner(winner_array)
-  # end
-
   def check_diagonal(number)
     winner_array = []
-    #winner = ""
     if number == 1
       custom_key_array = [0, 0, 0, 1, 2, 3]
     else
@@ -163,19 +99,27 @@ attr_reader :board, :game_over
   end
 
   def check_vertical
-    # binding.pry
     winner_array = []
-    winner = ""
     board.board_hash.keys.each do |key|
       winner_array << check_win_condition(board.board_hash[key])
     end
     check_winner(winner_array)
   end
 
+  def check_winner(winner_array)
+    if winner_array.include?("X")
+      winner = "X"
+    elsif winner_array.include?("O")
+      winner = "O"
+    else
+      winner = ""
+    end
+    return winner
+  end
+
   def check_win_condition(array)
   counter = 0
   winner = ""
-  # y_counter = 1
   array.each do
       if array[counter] == array[counter + 1] && array[counter] == array[counter + 2] && array[counter] == array[counter + 3] && array[counter] == 'X'
         winner = "X"
